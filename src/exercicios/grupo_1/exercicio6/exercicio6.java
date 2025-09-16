ffe6cf3280204b691f7cc0bf5ccd6f4d9bb363bf
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-16 12:01:47 -0300

    Alteraçao e compilaçao do exercicio 6

---
+++ src/exercicios/grupo_1/exercicio6/exercicio6.java
@@ -0,0 +1,30 @@
+/*
+  	Objetivo do programa: Calcule a área de um círculo com base no raio informado.
+							Como calcular: A = PI . R ao quadrado
+    Data da criacao: 2025-08-28
+    Criado por: @programacaomentoria
+	Ultima atualizacao: 2025-09-16
+    Alterado por: @rafaelfischer
+*/
+
+package exercicios.grupo_1.exercicio6;
+import java.util.Scanner;
+
+public class exercicio6 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		final double PI = 3.14159;
+		double raio, area;
+
+		System.out.println("Exercicio 6 - Calculo da area de um circulo");
+		System.out.println("------------------------------------------");
+
+		System.out.print("Informe o valor do raio: ");
+		raio = scanner.nextDouble();
+
+		area = PI * (raio * raio);
+		System.out.printf("A area do circulo de raio %.2f é: %.2f%n", raio, area);
+		
+		scanner.close();
+	}
+}
